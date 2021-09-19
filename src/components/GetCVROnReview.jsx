35b7d523e3957323e7c8e16852003a8e8840abe8
Shahir Sumra <shahir@audacelabs.com>
2021-09-20 00:15:28 +0530

    added menifesto page

--- src/components/GetCVROnReview.jsx
+++ src/components/GetCVROnReview.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const GetCVROnReview = () => {
   return (
     <>
-      <div className="absolute top-5 right-0 px-5 py-3 bg-getCVRBg text-white font-Montserrat font-semibold text-body-md underline">
+      <div className="absolute md:top-5 top-6 right-0 w-36 sm:w-auto md:px-5 md:py-3 px-2 py-1 bg-getCVRBg text-white font-Montserrat font-semibold md:text-body-md text-body-3xs underline">
         Submit a review and get 500 CVR token
       </div>
     </>

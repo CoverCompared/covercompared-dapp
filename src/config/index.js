d5d22dd706ae8ef008df6fceefd3f644b5afcf38
mobiledev-111-118 <mobiledev111118@gmail.com>
2021-09-23 21:53:16 -0700

    add function for integration

--- src/config/index.js
+++ src/config/index.js
@@ -8,3 +8,7 @@ export const NetworkContextName = 'NETWORK';
 export const BASE_URL = 'https://polkacover.finance';
 
 export const BASE_SCAN_URL = BASE_SCAN_URLS[42];
+
+export const BASE_API_STAGE_URL = 'https://staging-covercompared.polkacover.com/api/';
+
+export const BASE_API_PROD_URL = 'https://covercompared.polkacover.com/api';

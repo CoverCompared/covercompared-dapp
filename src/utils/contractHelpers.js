d8127d69ff1b6c316ff20500e608e1183cc0c599
mobiledev-111-118 <mobiledev111118@gmail.com>
2021-09-27 00:09:01 -0700

    added some files for integration with smart contract

--- src/utils/contractHelpers.js
+++ src/utils/contractHelpers.js
@@ -1,12 +1,12 @@
 import { ethers } from 'ethers';
-import { simpleRpcProvider } from 'utils/providers';
+import { simpleRpcProvider } from './providers';
 
 // Addresses
 import { getAddress, getCoverComparedAddress } from './addressHelpers';
 
 // ABI
-import erc20Abi from 'config/abi/erc20.json';
-import CovercomaredAbi from 'config/abi/covercompared.json';
+import erc20Abi from '../config/abi/erc20.json';
+import CovercomaredAbi from '../config/abi/covercompared.json';
 
 const getContract = (abi, address, signer) => {
   const signerOrProvider = signer ?? simpleRpcProvider;

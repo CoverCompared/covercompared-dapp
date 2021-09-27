f280db3daeebe8b76185a03742689af59e45155b
mobiledev-111-118 <mobiledev111118@gmail.com>
2021-09-26 23:51:06 -0700

    added some files for integration with smart contract

---
+++ src/utils/addressHelpers.js
@@ -0,0 +1,10 @@
+import addresses from '../config/contracts'
+
+export const getAddress = (address) => {
+  const chainId = process.env.REACT_APP_CHAIN_ID;
+  return address[chainId] ? address[chainId] : address[1];
+}
+
+export const getCoverComparedAddress = () => {
+  return getAddress(addresses.coverCompared);
+}
---
+++ src/utils/contractHelpers.js
@@ -0,0 +1,24 @@
+import { ethers } from 'ethers'
+import { simpleRpcProvider } from 'utils/providers'
+
+// Addresses
+import {
+  getAddress,
+  getCoverComparedAddress,
+} from './addressHelpers'
+
+// ABI
+import erc20Abi from 'config/abi/erc20.json'
+import CovercomaredAbi from 'config/abi/Cohort.json'
+
+const getContract = (abi, address, signer) => {
+  const signerOrProvider = signer ?? simpleRpcProvider
+  return new ethers.Contract(address, abi, signerOrProvider)
+}
+
+export const getErc20Contract = (address, signer) => {
+  return getContract(erc20Abi, address, signer)
+}
+export const getCovercomparedContract = (signer) => {
+  return getContract(CovercomaredAbi, getCoverComparedAddress(), signer)
+}

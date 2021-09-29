42b18e0709173040ad05314ffccdfee2b5909b2f
mobiledev-111-118 <mobiledev111118@gmail.com>
2021-09-29 10:06:43 -0700

    added web3 package and getting ethUSD price function

--- src/utils/getAssetPrice.js
+++ src/utils/getAssetPrice.js
@@ -1,15 +1,19 @@
-import { getPriceFeedAddressBySymbol } from "./addressHelpers";
-import { getPriceFeedContract } from "./contractHelpers";
+import { getPriceFeedAddressBySymbol } from './addressHelpers';
+import { getPriceFeedContract } from './contractHelpers';
 
-export default getAssetPriceBySymbol = (symbol) => {
-    const priceFeedAddress = getPriceFeedAddressBySymbol(symbol);
-    const priceFeedContract = getPriceFeedContract(priceFeedAddress);
+const getAssetPriceBySymbol = (symbol) => {
+  const priceFeedAddress = getPriceFeedAddressBySymbol(symbol);
+  const priceFeedContract = getPriceFeedContract(priceFeedAddress);
 
-    priceFeedContract.methods.latestRoundData().call()
-        .then((roundData) => {
-            console.log("Latest Round Data", roundData);
-        })
-        .catch((err) => {
-            console.error(err);
-        })
-}
+  priceFeedContract.methods
+    .latestRoundData()
+    .call()
+    .then((roundData) => {
+      console.log('Latest Round Data', roundData);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+};
+
+export default getAssetPriceBySymbol;

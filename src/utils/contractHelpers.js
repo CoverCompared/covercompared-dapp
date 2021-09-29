eb5bdada5e842e1c03bf5fd73884e2cac3086174
mobiledev-111-118 <mobiledev111118@gmail.com>
2021-09-29 10:04:15 -0700

    added web3 package and getting ethUSD price function

--- src/utils/contractHelpers.js
+++ src/utils/contractHelpers.js
@@ -1,4 +1,5 @@
 import { ethers } from 'ethers';
+import Web3 from 'web3';
 
 // Addresses
 import { getAddress, getCoverComparedAddress } from './addressHelpers';
@@ -6,6 +7,9 @@ import { getAddress, getCoverComparedAddress } from './addressHelpers';
 // ABI
 import erc20Abi from '../config/abi/erc20.json';
 import CovercomaredAbi from '../config/abi/covercompared.json';
+import AggregatorV3InterfaceABI from '../config/abi/aggregatorV3InterfaceABI.json';
+
+const web3 = new Web3("https://kovan.infura.io/v3/92a35c94033b48c6a8d248ac76e7650e");
 
 const getContract = (abi, address, signer) => {
   const simpleRpcProvider = ethers.getDefaultProvider('kovan');
@@ -16,6 +20,11 @@ const getContract = (abi, address, signer) => {
 export const getErc20Contract = (address, signer) => {
   return getContract(erc20Abi, address, signer);
 };
+
 export const getCovercomparedContract = (signer) => {
   return getContract(CovercomaredAbi, getCoverComparedAddress(), signer);
+};
+
+export const getPriceFeedContract = (address) => {
+ return new web3.eth.Contract(AggregatorV3InterfaceABI, address);
 };
---
+++ src/utils/getAssetPrice.js
@@ -0,0 +1,15 @@
+import { getPriceFeedAddressBySymbol } from "./addressHelpers";
+import { getPriceFeedContract } from "./contractHelpers";
+
+export const getAssetPriceBySymbol = (symbol) => {
+    const priceFeedAddress = getPriceFeedAddressBySymbol(symbol);
+    const priceFeedContract = getPriceFeedContract(priceFeedAddress);
+
+    priceFeedContract.methods.latestRoundData().call()
+        .then((roundData) => {
+            console.log("Latest Round Data", roundData);
+        })
+        .catch((err) => {
+            console.error(err);
+        })
+}

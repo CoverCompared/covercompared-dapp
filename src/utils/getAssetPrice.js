dc333ffa34586324db9701b7c901f802ff842aff
mobiledev-111-118 <mobiledev111118@gmail.com>
2021-10-01 10:01:44 -0700

    fix eslint issue

--- src/utils/getAssetPrice.js
+++ src/utils/getAssetPrice.js
@@ -2,8 +2,8 @@ import { getPriceFeedAddressBySymbol } from './addressHelpers';
 import { getPriceFeedContract } from './contractHelpers';
 
 export const getBalanceNumberByDecimal = (balance, decimals = 8) => {
-  return balance ? parseInt(balance[`_hex`], 16)/(10 ** decimals) : 0
-}
+  return balance ? parseInt(balance._hex, 16) / 10 ** decimals : 0;
+};
 
 const getAssetPriceBySymbol = async (symbol) => {
   const priceFeedAddress = getPriceFeedAddressBySymbol(symbol);

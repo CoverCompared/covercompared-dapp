659c86f1932f7cb1b0874bde55671aed8b9798d9
mobiledev-111-118 <mobiledev111118@gmail.com>
2021-10-01 09:41:10 -0700

    integrated getting usd price by asset symbol

--- src/utils/getAssetPrice.js
+++ src/utils/getAssetPrice.js
@@ -1,15 +1,15 @@
 import { getPriceFeedAddressBySymbol } from './addressHelpers';
 import { getPriceFeedContract } from './contractHelpers';
 
+export const getBalanceNumberByDecimal = (balance, decimals = 8) => {
+  return balance ? parseInt(balance[`_hex`], 16)/(10 ** decimals) : 0
+}
+
 const getAssetPriceBySymbol = async (symbol) => {
   const priceFeedAddress = getPriceFeedAddressBySymbol(symbol);
   const priceFeedContract = getPriceFeedContract(priceFeedAddress);
   const res = await priceFeedContract.latestRoundData();
-  return getBalanceNumberByDecimal(res.answer);
+  return res ? getBalanceNumberByDecimal(res.answer) : 0;
 };
-
-export const getBalanceNumberByDecimal = (balance, decimals = 8) => {
-  return balance ? parseInt(balance._hex, 16)/(10 ** decimals) : 0
-}
 
 export default getAssetPriceBySymbol;
